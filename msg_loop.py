daf1e89458f7be769f9b8d467325bb5853306b25
Rodrigo Oliveira ( Digão ) <49177275+Plague14@users.noreply.github.com>
2023-08-09 10:32:42 -0300

    commit 1

---
+++ msg_loop.py
@@ -0,0 +1,41 @@
+def MsgLoopSender(msgs, qnt):
+    import pynput, random, time
+    from pynput.keyboard import Key, Controller
+    import math
+    
+    keyboard = Controller()
+    print("Iniciando em 5s")
+    time.sleep(1)
+    print("Iniciando em 4s")
+    time.sleep(1)
+    print("Iniciando em 3s")
+    time.sleep(1)
+    print("Iniciando em 2s")
+    time.sleep(1)
+    print("Iniciando em 1s")
+    time.sleep(1)
+    print("Iniciado !")    
+    for i in range(0, qnt):
+        W = random.randint(0, len(msgs)-1)
+        for c in msgs[W]:
+            keyboard.press(c)
+            keyboard.release(c)
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
+        time.sleep(0.1)
+
+#msg = ["Je t’aime", "Eu te amo","Ti voglio bene","Ti amo","Te quiero","I love you","Seni seviyorum","Te dua","Ek het jou liefe or Ek is lief vir jou","Wo ai ni","S’agapo","Ana behibek","Volim te","Mahal kita"]
+msg = ['seu saymonista','toma gank do digão pow pow','Muito querido por elas!']
+qnt = 140 #Numero do capeta usa no máximo 300 já é coisa pra caralho
+#msg = ['viado','chupa pau','gay','arrombado','atende','acorda']
+'''f = open("f:\PYTHON/101.txt","r")
+
+#msg = []
+for i in f:
+    msg.append(i.split("=")[1][1::].replace("\n",""))'''
+msgreal = []
+for i in range(0, len(msg)):
+    msgreal.append(msg[i])
+
+
+MsgLoopSender(msgreal, qnt)
